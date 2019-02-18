7cff8e0b70e95720d0308e559479867f5bf6cb7e
MARKENVY-HP\cgmmr <mark.h.mackey@gmail.com>
2019-02-18 11:22:46 -0800

    Retrieve all database info and use JS to filter.

--- _scripts/academics.js
+++ _scripts/academics.js
@@ -1,7 +1,14 @@
 /*
- * MHM 2019-01-17
+ * MHM 2019-02-17
  * Comment:
  *  Retrieve academic information from the database.
+ * 
+ * MHM 2019-02-18
+ * Comment:
+ *  Simplify database lookup to return all records and then use
+ *  javascript to get the information we are after. Since we're no
+ *  longer doing a specfic database lookup, we'll change the HTTP
+ *  request to a GET instead of a POST.
  */
 
 let returned_data;
@@ -10,20 +17,41 @@ let returned_data;
  * MHM 2019-02-17
  * Comment:
  *  Callback function to assign return value from AJAX call
+ * 
+ * MHM 2019-02-18
+ * Comment:
+ *  Removed database parameters since we are now doing a full table
+ *  data request.
  */
 
-let performSomeAction = function(returned_data) {
-  let txt="";
+let performSomeAction = function(returned_data, season, year, student) {
   /*
    * MHM 2019-02-17
    * Comment:
    *  Find the table in the HTML document and build it.
+   * 
+   * MHM 2019-02-18
+   * Comment:
+   *  Added the season, year and student parameters since we're going
+   *  doing the filtering here instead of in the database query.
    */
   let tableElement = document.getElementById("semTab");
   let row;
   let cnt=1;
   let cell1, cell2, cell3, cell4;
-  let semesterList = returned_data.semesterList;
+
+  /*
+   * MHM 2019-02-18
+   * Comment:
+   *  We get the complete transcript list from the database. Now
+   *  Filter for the records we want to display.
+   */
+
+  let transcriptList = returned_data.transcriptList;
+  let semesterList = transcriptList.filter(function (obj) {
+    return (obj.season===season && obj.year===year && obj.studentName===student);
+  });
+
   for (x in semesterList) {
     row = tableElement.insertRow(cnt++);
     row.className = "academicEntry";
@@ -60,15 +88,22 @@ function getAcademicInfo(callback, season, year, student) {
    * MHM 2019-02-17
    * Comment:
    *  Build parameter list for db query
+   * 
+   * MHM 2019-02-18
+   * Comment:
+   *  Removed DB query parameters.
    */
-  let myObj = { "season":season, "year":year, "student":student};
-  let dbParm = JSON.stringify(myObj);
 
   /*
    * MHM 2019-02-17
    * Comment:
    *  Set-up and call AJAX POST request, provide a callback routine
    *  for the results
+   * 
+   * MHM 2019-02-18
+   * Comment:
+   *  Since we removed the DB parameters, change the POST request to
+   *  a GET request.
    */
   let xhttp = new XMLHttpRequest();
 
@@ -78,14 +113,18 @@ function getAcademicInfo(callback, season, year, student) {
        * MHM 2019-02-17
        * Comment: 
        *  Call callback function and send result as an array 
+       * 
+       * MHM 2019-02-18
+       * Comment:
+       *  Add season, year and student to callback. We'll use
+       *  javascript to filter the information we're looking for.
        */
-      callback.apply(this,[JSON.parse(this.responseText)]);
+      callback.apply(this,[JSON.parse(this.responseText), season, year, student]);
     }
   };
 
-  xhttp.open("POST", "academics.php", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("x=" + dbParm);
+  xhttp.open("GET", "academics.php", true);
+  xhttp.send();
 }
 
 /*
@@ -104,6 +143,10 @@ function removeElementsByClassName(className) {
  * MHM 2019-02-17
  * Comment:
  *  Main logic
+ * 
+ * MHM 2019-02-18
+ * Comment:
+ *  Change 2010 to "2010". The database year field is a string.
  */
 removeElementsByClassName("academicEntry");
-getAcademicInfo(performSomeAction, "FALL", 2010, "Rachel");
+getAcademicInfo(performSomeAction, "FALL", "2011", "Rachel");
